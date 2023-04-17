a6d3bb764464d0dead57ef520c953c6f1f97a74f
elizabetabeta <elizabeta.milicevic7@gmail.com>
2023-04-17 12:48:55 +0200

    novi commit

---
+++ src/main/java/ba/sum/fpmoz/restoran2/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package ba.sum.fpmoz.restoran2.repositories;
+
+import ba.sum.fpmoz.restoran2.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends
+        JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    public User findByEmail(String email);
+}
